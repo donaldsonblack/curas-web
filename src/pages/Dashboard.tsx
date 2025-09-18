f661e36798e130cd71f13ff109162f22d54d2d6f
stirlingwdonaldson <s4169686@student.rmit.edu.au>
2025-09-18 20:53:37 +1000

    added simple styling plan for row of cards in dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,15 @@
+import { Card } from "@/components/ui/card";
 
 export default function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <>
+      <div className="h-screen border-red-500 border-2 ">
+        <div className="block sm:flex sm:justify-between p-4 space-y-1">
+          <div className="w-full sm:w-[32%] bg-gray-200 p-4">Card 1</div>
+          <div className="w-full sm:w-[32%] bg-gray-200 p-4">Card 2</div>
+          <div className="w-full sm:w-[32%] bg-gray-200 p-4">Card 3</div>
+        </div>
+      </div>
+    </>
   );
 }
-
