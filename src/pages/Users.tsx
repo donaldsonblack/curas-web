2d4bf52f7c0daaf511b1474c5c952211f6b27374
stirlingwdonaldson <s4169686@student.rmit.edu.au>
2025-09-18 12:48:47 +1000

    cleared users page

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,12 +1,7 @@
-import UserTable from "../components/tables/UserTable";
-
 export default function UsersPage() {
   return (
-    <div className="container mx-auto p-4 sm:p-6 lg:p-8">
-      <h1 className="mb-4 text-2xl font-semibold">Users</h1>
-      <div className="overflow-x-auto">
-        <UserTable />
-      </div>
+    <div>
+      <h1>Users</h1>
     </div>
   );
 }
