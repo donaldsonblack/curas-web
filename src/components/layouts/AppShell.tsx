a7999bf2ed08f41fc3838b44ad4b785dcf2de762
stirlingwdonaldson <s4169686@student.rmit.edu.au>
2025-10-01 19:13:09 +1000

    added sonner for when card is clicked in dashboard page

--- src/components/layouts/AppShell.tsx
+++ src/components/layouts/AppShell.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom"
 import { AppSidebar } from "../ui/app-sidebar"
 import { SiteHeader } from "../ui/site-header"
+import { Toaster } from "../ui/sonner"
 import {
   SidebarInset,
   SidebarProvider,
@@ -16,6 +17,7 @@ export default function AppShell() {
           <Outlet /> {/* This renders the nested route components like Home or Info */}
         </SidebarInset> 
       </SidebarProvider>
+      <Toaster />
     </>
   )
 }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,13 +1,50 @@
 import { Card } from "@/components/ui/card";
+import { toast } from "sonner";
 
 export default function Dashboard() {
+  const handleCard1Click = () => {
+    toast.success("Card 1 clicked!", {
+      description: "This is the first card action",
+      position: "bottom-right",
+    });
+  };
+
+  const handleCard2Click = () => {
+    toast.info("Card 2 clicked!", {
+      description: "This is the second card action", 
+      position: "bottom-right",
+    });
+  };
+
+  const handleCard3Click = () => {
+    toast.warning("Card 3 clicked!", {
+      description: "This is the third card action",
+      position: "bottom-right", 
+    });
+  };
+
   return (
     <>
       <div className="p-4">
         <div className="flex flex-row flex-wrap gap-5 justify-center">
-          <Card className="p-4 w-full lg:w-[30%]">CARD</Card>
-          <Card className="p-4 w-full lg:w-[30%]">CARD</Card>
-          <Card className="p-4 w-full lg:w-[30%]">CARD</Card>
+          <Card 
+            className="p-4 w-full lg:w-[30%] cursor-pointer hover:shadow-lg transition-shadow" 
+            onClick={handleCard1Click}
+          >
+            CARD 1
+          </Card>
+          <Card 
+            className="p-4 w-full lg:w-[30%] cursor-pointer hover:shadow-lg transition-shadow" 
+            onClick={handleCard2Click}
+          >
+            CARD 2
+          </Card>
+          <Card 
+            className="p-4 w-full lg:w-[30%] cursor-pointer hover:shadow-lg transition-shadow" 
+            onClick={handleCard3Click}
+          >
+            CARD 3
+          </Card>
         </div>
       </div>
     </>
