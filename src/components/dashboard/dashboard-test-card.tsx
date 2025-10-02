89415f63465d8c2329bbc0224c7edaf2d6c435d8
stirlingwdonaldson <s4169686@student.rmit.edu.au>
2025-10-02 16:59:29 +1000

    populated test card

--- src/components/dashboard/dashboard-test-card.tsx
+++ src/components/dashboard/dashboard-test-card.tsx
@@ -1,10 +1,20 @@
-import { Card } from "../ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
 
 export default function DashboardTestCard() {
     return (
         <div className="h-full">
-            <Card className="h-full flex items-center justify-center">
-                SIMPLE CARD
+            <Card className="h-full flex flex-col">
+                <CardHeader className="flex-shrink-0">
+                    <CardTitle>
+                        HEADER
+                    </CardTitle>
+                    <CardDescription>
+                        DESCRIPTION
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className="flex-1 flex items-center justify-center">
+                    CONTENT
+                </CardContent>
             </Card>
         </div>
     );
