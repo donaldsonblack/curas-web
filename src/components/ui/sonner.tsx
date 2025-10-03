cf2fe67249aecdc7904c8a63e18d9901a3dacead
stirlingwdonaldson <s4169686@student.rmit.edu.au>
2025-10-03 11:18:03 +1000

    updated redundant import and declare statements in dashboard-kpi-cards and sonner that were causing build issues

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,9 +1,7 @@
-import { useTheme } from "next-themes"
 import { Toaster as Sonner } from "sonner"
 import type { ToasterProps } from "sonner"
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
 
   return (
     <>
