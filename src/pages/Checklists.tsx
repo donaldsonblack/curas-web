461a97a44e79bd0028210f578883e048e3410a08
stirlingwdonaldson <s4169686@student.rmit.edu.au>
2025-10-03 11:56:45 +1000

    started populating checklists page with basic section with title and body of cards that will be for checklists

--- src/pages/Checklists.tsx
+++ src/pages/Checklists.tsx
@@ -1,7 +1,22 @@
+import { Card } from "@/components/ui/card";
+
 export default function Checklists() {
   return (
-    <div>
-      <h1>Checklists</h1>
+    <div className="p-4">
+      <div className="flex flex-col gap-5 bg-gray-300 p-4 ">
+
+        <div className="bg-green-300">
+          <h1 className="">CHECKLISTS</h1>
+        </div>
+
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+
+          <Card className="cursor-pointer hover:shadow-lg transition-shadow">
+          </Card>
+
+        </div>
+
+      </div>
     </div>
   );
 }
